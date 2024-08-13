2dd3e31f7b93e4ca288a7c2dbb029ab21dafd681
iori3662 <7522111@ed.tus.ac.jp>
2024-08-13 17:18:24 +0900

    100Hz change

--- wireless_GPS/wireless_GPS.ino
+++ wireless_GPS/wireless_GPS.ino
@@ -1,20 +1,18 @@
 #include <SPI.h>
 #include <string.h>
-#include <TimerTCC0.h>
-#include <TimerTC3.h>
-
 #include <CCP_MCP2515.h>
 
-#define CAN0_CS 0
-#define CAN0_INT 1
+#define CAN0_CS D0
+#define CAN0_INT D1
 
 CCP_MCP2515 CCP(CAN0_CS, CAN0_INT);
 
-bool is100hz = false;
-bool is1hz = false;
 
 uint32_t data_A_GNSS_latitude_udeg = 0;
 uint32_t data_A_GNSS_longitude_udeg = 0;
+
+const unsigned long interval = 1000;  // 処理の間隔を10ミリ秒（100Hz）に設定
+unsigned long previousMillis = 0;   // 前回の処理時間を保存する変数
 
 void setup() {
   Serial.begin(115200);
@@ -25,18 +23,12 @@ void setup() {
   digitalWrite(CAN0_CS, HIGH);
   CCP.begin();
 
-  // CAN用タイマー
-  TimerTcc0.initialize(10000);  // 10,000us=100Hz
-  TimerTcc0.attachInterrupt(TimerIsr);
-
-  // ダウンリンク用タイマー
-  TimerTc3.initialize(1000000);
-  TimerTc3.attachInterrupt(TimerCnt);
-
   delay(100);
 }
 
 void loop() {
+  unsigned long currentMillis = millis();  // 現在の時間を取得
+
   // 地上局へのテレメトリ
   if (!digitalRead(CAN0_INT))  // データ受信確認
   {
@@ -53,29 +45,15 @@ void loop() {
     }
   }
 
-  if (is100hz) {
-    is100hz = false;
-  }
-
-  if (is1hz) {
-    is1hz = false;
+  if (currentMillis - previousMillis >= interval) {
+    previousMillis = currentMillis;  // 前回の処理時間を現在の時間に更新
 
     Serial1.print(data_A_GNSS_latitude_udeg);
     Serial1.print(",");
     Serial1.println(data_A_GNSS_longitude_udeg);
-  }
-}
 
-void TimerIsr() {
-  if (is100hz) {
-    Serial.println("overrun");
+    Serial.print(data_A_GNSS_latitude_udeg);
+    Serial.print(",");
+    Serial.println(data_A_GNSS_longitude_udeg);
   }
-  is100hz = true;
-}
-
-void TimerCnt() {
-  if (is1hz) {
-    Serial.println("overrun10hz");
-  }
-  is1hz = true;
 }
