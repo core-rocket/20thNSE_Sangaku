34fd03f6565319f95f142540774042220e7aa80c
maruyama0630 <138867152+maruyama0630@users.noreply.github.com>
2024-08-15 02:48:28 +0900

    add_mpxv5100

--- bme680_getData/bme680_getData.ino
+++ bme680_getData/bme680_getData.ino
@@ -27,22 +27,27 @@
 // CAN
 CCP_MCP2515 CCP(CAN0_CS, CAN0_INT);
 
-#define BME_SCK 13
-#define BME_MISO 12
-#define BME_MOSI 11
-#define BME_CS 10
-
+//BME680
 float altitude_m = 0;
 float temperature_c = 0;
-float humidity_per= 0;
+float humidity_per = 0;
 float pressure_hPa = 0;
-float gas_KOhms=0;
+float gas_KOhms = 0;
 
 #define SEALEVELPRESSURE_HPA (1013.25)
 
 Adafruit_BME680 bme;  // I2C
 //Adafruit_BME680 bme(BME_CS); // hardware SPI
 //Adafruit_BME680 bme(BME_CS, BME_MOSI, BME_MISO,  BME_SCK);
+
+//MPXV5100
+float IN_min = 0.2;
+float IN_max = 4.7;
+float OUT_min = 0;
+float voltage = 0;
+float outputkPa = 0;
+float outputmmH2O = 0;
+int sensorValue = 0;
 
 void setup() {
   Serial.begin(115200);
@@ -71,23 +76,37 @@ void loop() {
     Serial.println("Failed to perform reading :(");
     return;
   }
+  //MPXV5100
+  sensorValue = analogRead(A7);  // XIAO_SAMD
+  voltage = sensorValue * (3.3 / 1023.0);
+  outputkPa = fmap(voltage, IN_min, IN_max, OUT_min, 10);
+  outputmmH2O = fmap(voltage, IN_min, IN_max, OUT_min, 1019.78);
+  // print out the values on serial monitor
+  Serial.print("Volt: ");
+  Serial.println(voltage);
+  Serial.print("kPa: ");
+  Serial.println(outputkPa);
+  Serial.print("mmH2O: ");
+  Serial.println(outputmmH2O);
+  Serial.println();
+  //BME680
   Serial.print("Temperature = ");
   temperature_c = bme.temperature;
   Serial.print(temperature_c);
   Serial.println(" *C");
 
   Serial.print("Pressure = ");
-  pressure_hPa=bme.pressure/100.0;
+  pressure_hPa = bme.pressure / 100.0;
   Serial.print(pressure_hPa);
   Serial.println(" hPa");
 
   Serial.print("Humidity = ");
- humidity_per= bme.humidity;
+  humidity_per = bme.humidity;
   Serial.print(humidity_per);
   Serial.println(" %");
 
   Serial.print("Gas = ");
-  gas_KOhms=bme.gas_resistance / 1000.0;
+  gas_KOhms = bme.gas_resistance / 1000.0;
   Serial.print(gas_KOhms);
   Serial.println(" KOhms");
 
@@ -95,7 +114,10 @@ void loop() {
   altitude_m = bme.readAltitude(SEALEVELPRESSURE_HPA);
   Serial.print(altitude_m);
   Serial.println(" m");
-
+  //Data_to_CAN
+  CCP.float_to_device(CCP_nose_voltage, voltage);
+  CCP.float_to_device(CCP_nose_outputkPa, outputkPa);
+  CCP.float_to_device(CCP_nose_outputmmH2O, outputmmH2O);
   CCP.float_to_device(CCP_nose_pressure_hPa, pressure_hPa);
   CCP.float_to_device(CCP_nose_temperature_C, temperature_c);
   CCP.float_to_device(CCP_nose_humidity_percent, humidity_per);
@@ -103,4 +125,7 @@ void loop() {
   CCP.float_to_device(CCP_nose_gas_KOhms, gas_KOhms);
 
   Serial.println();
+}
+float fmap(float x, float in_min, float in_max, float out_min, float out_max) {
+  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
 }
