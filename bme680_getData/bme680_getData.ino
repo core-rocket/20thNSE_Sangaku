e243fbc0480c9d3a0dcffce9de295f9e2564d6b2
maruyama0630 <138867152+maruyama0630@users.noreply.github.com>
2024-08-14 23:24:42 +0900

    CAN実装

--- bme680_getData/bme680_getData.ino
+++ bme680_getData/bme680_getData.ino
@@ -25,20 +25,28 @@
 #define BME_MOSI 11
 #define BME_CS 10
 
+float altitude_m = 0;
+float temperature_c = 0;
+float humidity_%= 0;
+float pressure_hPa = 0;
+float gas_KOhms=0;
+
 #define SEALEVELPRESSURE_HPA (1013.25)
 
-Adafruit_BME680 bme; // I2C
+Adafruit_BME680 bme;  // I2C
 //Adafruit_BME680 bme(BME_CS); // hardware SPI
 //Adafruit_BME680 bme(BME_CS, BME_MOSI, BME_MISO,  BME_SCK);
 
 void setup() {
   Serial.begin(115200);
   Wire.begin();
-  while (!Serial);
+  while (!Serial)
+    ;
   Serial.println(F("BME680 test"));
   if (!bme.begin()) {
     Serial.println("Could not find a valid BME680 sensor, check wiring!");
-    while (1);
+    while (1)
+      ;
   }
 
   // Set up oversampling and filter initialization
@@ -46,33 +54,44 @@ void setup() {
   bme.setHumidityOversampling(BME680_OS_2X);
   bme.setPressureOversampling(BME680_OS_4X);
   bme.setIIRFilterSize(BME680_FILTER_SIZE_3);
-  bme.setGasHeater(320, 150); // 320*C for 150 ms
+  bme.setGasHeater(320, 150);  // 320*C for 150 ms
 }
 
 void loop() {
-  if (! bme.performReading()) {
+  if (!bme.performReading()) {
     Serial.println("Failed to perform reading :(");
     return;
   }
   Serial.print("Temperature = ");
-  Serial.print(bme.temperature);
+  temperature_c = bme.temperarure;
+  Serial.print(temperature_c);
   Serial.println(" *C");
 
   Serial.print("Pressure = ");
-  Serial.print(bme.pressure / 100.0);
+  pressure_hPa=bme.pressure/100.0;
+  Serial.print(pressure_hPa);
   Serial.println(" hPa");
 
   Serial.print("Humidity = ");
-  Serial.print(bme.humidity);
+  humidity_%= bme.humidity;
+  Serial.print(humidity_%);
   Serial.println(" %");
 
   Serial.print("Gas = ");
-  Serial.print(bme.gas_resistance / 1000.0);
+  gas_KOhms=bme.gas_resistance / 1000.0;
+  Serial.print(gas_KOhms);
   Serial.println(" KOhms");
 
   Serial.print("Approx. Altitude = ");
-  Serial.print(bme.readAltitude(SEALEVELPRESSURE_HPA));
+  altitude_m = bme.readAltitude(SEALEVELPRESSURE_HPA);
+  Serial.print(altitude_m);
   Serial.println(" m");
+
+  CCP.float_to_device(CCP_nose_pressure_hPa, pressure_hPa);
+  CCP.float_to_device(CCP_nose_temperature_C, temperature_c);
+  CCP.float_to_device(CCP_nose_humidity_percent, humidity_%);
+  CCP.float_to_device(CCP_nose_altitude_m, altitude_m);
+  CCP.float_to_device(CCP_nose_altitude_m, gas_KOhms);
 
   Serial.println();
 }
