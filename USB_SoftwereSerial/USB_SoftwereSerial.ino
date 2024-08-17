30368360a4b62b77ba0614865e421528711975ce
iori3662 <7522111@ed.tus.ac.jp>
2024-08-17 18:11:41 +0900

    修正　カッコ消した

--- USB_SoftwereSerial/USB_SoftwereSerial.ino
+++ USB_SoftwereSerial/USB_SoftwereSerial.ino
@@ -7,10 +7,6 @@
 // Set up a new SoftwareSerial object
 SoftwareSerial mySerial = SoftwareSerial(rxPin, txPin);
 
-
-String downlink1 = "downlink1";
-String downlink2 = "downlink2";
-
 void setup() {
   // Define pin modes for TX and RX
   pinMode(rxPin, INPUT);
@@ -19,22 +15,18 @@ void setup() {
   // Set the baud rate for the SoftwareSerial object
   mySerial.begin(115200);
   Serial.begin(115200);
-  Serial1.begin(115200);
-
-  delay(5000);
 }
-
 
 void loop() {
   if (mySerial.available() > 0) {                //downlink
-    downlink1 = mySerial.readStringUntil('\n');  // 受信した文字列を読み取ります
+    String downlink1 = mySerial.readStringUntil('\n');  // 受信した文字列を読み取ります
     downlink1.trim();
     Serial.println(downlink1);
   }
 
-  if (Serial1.available() > 0) {                //GPS
-    downlink2 = Serial1.readStringUntil('\n');  // 受信した文字列を読み取ります
+  if (Serial.available() > 0) {                //GPS
+    String downlink2 = Serial.readStringUntil('\n');  // 受信した文字列を読み取ります
     downlink2.trim();
-    Serial.println(downlink2);
+    mySerial.println(downlink2);
   }
 }
